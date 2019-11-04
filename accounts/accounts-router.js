0128cb6792f9f9e37a1dc7d19c0288a1bb7b7f2c
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-04 13:47:29 -0800

    added handler for post

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -11,12 +11,23 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-
     try {
         const accounts = await db.select('*').from('accounts').where({ id: req.params.id });
         res.status(200).json(accounts);
     } catch (error) {
         res.status(500).json({ message: "Error getting accounts." });
+    }
+});
+
+router.post("/", async (req, res) => {
+    if (!req.body) { res.status(400).json({ message: "Missing information" }); }
+    try {
+        const newId = await db
+            .insert(req.body, 'id')
+            .into('accounts');
+        res.status(200).json(newId);
+    } catch (error) {
+        res.status(500).json({ message: "Error adding new account." });
     }
 });
 
